2c97d88284eff7beb5d73e2cd135e52041b06696
clanni9081 <64356493+clanni9081@users.noreply.github.com>
2020-12-19 10:12:06 +0800

    Update feedforward.m

--- FNN/feedforward.m
+++ FNN/feedforward.m
@@ -7,14 +7,14 @@ global var;
     var.a_1 = max(z_1,0);
     var.jac_1 = var.a_1 > 0;
     
-    z_2 = var.hidden_layer_1(:,:,1) * var.a_1 + kron(var.hidden_layer_1(:,1),ones(1,sys.batch_size));
+    z_2 = var.hidden_layer_1(:,:,1) * var.a_1 + kron(var.hidden_layer_1_bias(:,1),ones(1,sys.batch_size));
     var.a_2 = max(z_2,0);
     var.jac_2 = var.a_2 > 0;
     
-    z_3 = var.hidden_layer_2(:,:,1) * var.a_2 + kron(var.hidden_layer_2(:,1),ones(1,sys.batch_size));
+    z_3 = var.hidden_layer_2(:,:,1) * var.a_2 + kron(var.hidden_layer_2_bias(:,1),ones(1,sys.batch_size));
     var.a_3 = max(z_3,0);
     var.jac_3 = var.a_3 > 0; 
     
-    z_4 = var.output_layer(:,:,1) * var.a_3 + kron(var.output_layer(:,1),ones(1,sys.batch_size));
+    z_4 = var.output_layer(:,:,1) * var.a_3 + kron(var.output_layer_bias(:,1),ones(1,sys.batch_size));
     a_4 = 1./(1+exp(-z_4));
 end
